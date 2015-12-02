33f60795707d042cea4c87a207aad4e913a03297
ckelner <chris.kelner@gmail.com>
2015-12-02 11:09:04 -0500

    wrap ha proxy and customize a bit

--- cookbooks/myhaproxy/metadata.rb
+++ cookbooks/myhaproxy/metadata.rb
@@ -5,3 +5,5 @@ license 'all_rights'
 description 'Installs/Configures myhaproxy'
 long_description 'Installs/Configures myhaproxy'
 version '0.1.0'
+
+depends 'haproxy', '~> 1.6.6'
--- cookbooks/myhaproxy/recipes/default.rb
+++ cookbooks/myhaproxy/recipes/default.rb
@@ -3,3 +3,14 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+
+node['haproxy']['members'] = [
+  {
+    'hostname' => 'ec2-52-91-184-195.compute-1.amazonaws.com',
+    'ipaddress' => '52.91.184.195',
+    'port' => 80,
+    'ssl_port' => 80
+  }
+]
+
+include_recipe 'haproxy::default'
